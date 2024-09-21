a75e9e93d267c524ab8bbe98493209bf2cf2a31d
TheDamStex <datenteys@gmail.com>
2024-09-22 02:19:30 +0300

    22.09.2024

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("menuButton").onclick = function() {
+    document.getElementById("menu").style.display = "block";
+};
+
+document.getElementById("closeButton").onclick = function() {
+    document.getElementById("menu").style.display = "none";
+};
